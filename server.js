cad2edc802c9e3591b30e60b72cfc570579ac80b
Vasyl Khrystynych <khrystynych.vasyl@gmail.com>
2019-11-12 10:40:51 +0200

    start

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(express.static(__dirname + "/dist/angular-nodejs-site"));
+server.get('*',function(req,res){
+  res.sendFile(path.join(__dirname + 'index.html'))
+})
+
+server.listen(PORT, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`listen:${PORT}`);
+  }
+});
