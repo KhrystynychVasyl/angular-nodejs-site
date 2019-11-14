41b03a65c25dd72654a87ae77e687b6d0bd13eb3
Vasyl Khrystynych <khrystynych.vasyl@gmail.com>
2019-11-14 18:31:53 +0200

    todo-css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from "./components/home/home.component";
 import { TodoComponent } from "./components/todo/todo.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "todo", pathMatch: "full" },
+  { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "todo", component: TodoComponent }
 ];
